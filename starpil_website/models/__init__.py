cd8593102e7a5f98ce4c277bb2be96c0ce752b04
sheveioncallender-boop <sheveioncallender@gmail.com>
2026-08-22 16:27:28 -0400

    Delete starpil_website directory

--- starpil_website/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import pro_application
-from . import product_public_category
-from . import product_template
-from . import res_config_settings
-from . import sale_order
-from . import website
